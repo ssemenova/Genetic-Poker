5e3b09282523eff7bb4c927e5ac59845d95952f9
Sofiya Semenova <novasofiya@gmail.com>
2016-02-02 15:45:21 -0500

    save

--- algorithm.py
+++ algorithm.py
@@ -2,19 +2,31 @@ from hand import hand
 import random
 
 def main():
-    population = []
+    # loop through generations
+    for gen in range(200):
+        population = []
 
-    # generate population of 200 hands
-    for i in range(1):
-        population.append(hand())
+        # Step 1: generate population of 200 hands
+        for i in range(200):
+            population.append(hand())
 
-    # for each hand, calculate its fitness
-    for currentHand in population:
-        currentHand.calculateFitness()
+        # Step 2: for each hand, calculate its fitness
+        fitnessList = []
+        fitnessAvg = 0
+        currScore = 0
 
-    # mutate = random.randrange(1,10) == 1
-    # if mutate:
-    #     mutate(population[i])
+        for currentHand in population:
+            currScore = currentHand.calculateFitness()
+            fitnessList.append(currScore)
+            fitnessAvg += currScore
+
+        fitnessAvg = fitnessAvg / 200
+        print 'Fitness Score for generation ' + str(gen) + ' = ' + str(fitnessAvg)
+
+
+        # mutate = random.randrange(1,10) == 1
+        # if mutate:
+        #     mutate(population[i])
 
 
 main()
